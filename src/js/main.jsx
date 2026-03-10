9a5d608b7b3e0c4d07324908533dbb1d72877bd1
Alexis Rodriguez <alexisrrh123@gmail.com>
2026-03-10 13:40:33 +0000

    ahora si terminado

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -18,29 +18,9 @@ let num3 = 0;
 let num4 = 0;
 let num5 = 0;
 let num6 = 0;
-
-const reiniciar = () => {
-  num1 = 0;
-  num2 = 0;
-  num3 = 0;
-  num4 = 0;
-  num5 = 0;
-  num6 = 0;
-
-}
-
-let contador = setInterval(function () {
-  num1;
-  num1 < 9 ? num1++ : num1 = 0;
-  num1 === 0 && (num2 < 9 ? num2++ : num2 = 0)
-  num1 === 0 && num2 === 0 && (num3 < 9 ? num3++ : num3 = 0)
-  num1 === 0 && num2 === 0 && num3 === 0 && (num4 < 9 ? num4++ : num4 = 0)
-  num1 === 0 && num2 === 0 && num3 === 0 && num4 === 0 && (num5 < 9 ? num5++ : num5 = 0)
-  num1 === 0 && num2 === 0 && num3 === 0 && num4 === 0 && num5 === 0 && (num6 < 9 ? num6++ : num6 = 0)
-
-
-
-  ReactDOM.createRoot(document.getElementById('root')).render(
+let contador;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+const renderizar=()=>{root.render(
     <React.StrictMode>
       <Home digito1={num1}
         digito2={num2}
@@ -50,11 +30,41 @@ let contador = setInterval(function () {
         digito6={num6} />
       <ButtonActivo onReiniciar={reiniciar}
       onParar={parar} />
-    </React.StrictMode>,
-  )
-}, 1000);
+    </React.StrictMode>,)}
+
+
+
+const iniciar=()=>{
+
+  contador = setInterval(function () {
+  
+  num1 < 9 ? num1++ : num1 = 0;
+  num1 === 0 && (num2 < 9 ? num2++ : num2 = 0)
+  num1 === 0 && num2 === 0 && (num3 < 9 ? num3++ : num3 = 0)
+  num1 === 0 && num2 === 0 && num3 === 0 && (num4 < 9 ? num4++ : num4 = 0)
+  num1 === 0 && num2 === 0 && num3 === 0 && num4 === 0 && (num5 < 9 ? num5++ : num5 = 0)
+  num1 === 0 && num2 === 0 && num3 === 0 && num4 === 0 && num5 === 0 && (num6 < 9 ? num6++ : num6 = 0)
+
+
+renderizar();
+  
+ }, 1000)}
 
 const parar = () => {
   clearInterval(contador);
 };
 
+const reiniciar = () => {
+  clearInterval(contador)
+  num1 = 0;
+  num2 = 0;
+  num3 = 0;
+  num4 = 0;
+  num5 = 0;
+  num6 = 0;
+iniciar();
+renderizar();
+
+}
+renderizar();
+iniciar();
